7e0c71a4d1337cf20150b81f0bdfd9f943bf8efd
Yulia <iuliiabarbash@gmail.com>
2021-07-21 20:45:08 -0400

    updated

--- src/__tests__/employeeManager.test.ts
+++ src/__tests__/employeeManager.test.ts
@@ -1,3 +1,4 @@
+import { isEnumMember } from "typescript";
 import { EmployeeHandler } from "./pageObjects/EmployeeHandler";
 
 const em = new EmployeeHandler();
@@ -39,4 +40,44 @@ describe("Employee Manager", () => {
       title: "Grand Poobah",
     });
   });
+
+
+describe("New block chalange 3 requirements", () =>{
+  test("Add new employee", async () => {
+    await em.addEmployee();
+    await em.selectEmployeeByName("New Employee");
+    await em.editEmployee({
+      name: "Yulia Barbash",
+      phone: "1233800",
+      title: "QA",
+    });
+    await em.saveChanges();
+    await em.selectEmployeeByName("Dollie Berry");
+    await em.selectEmployeeByName("Yulia Barbash");
+    let newEmployee = await em.getEmployeeInfo();
+    expect(newEmployee.name).toEqual("Yulia Barbash");
+    expect(newEmployee.phone).toEqual("1233800");
+    expect(newEmployee.title).toEqual("QA");
+  });
+  test("Cancel an edit of new employee", async () => {
+    await em.selectEmployeeByName("Bernice Ortiz");
+    await em.editEmployee({name: "Yulia Barbash"});
+    await em.cancelChanges();
+    await em.selectEmployeeByName("Phillip Weaver");
+    await em.selectEmployeeByName("Bernice Ortiz");
+    let employee2 = await em.getEmployeeInfo();
+    expect(employee2.name).toBe("Bernice Ortiz")
+  });
+  test("when editing and then navigating away without saving does not save changes", async () => {
+    await em.selectEmployeeByName("Bernice Ortiz");
+    await em.editEmployee({ 
+      title: "Custyomer support enginner",
+      name: "Yulia Barbash",});
+    await em.selectEmployeeByName("Phillip Weaver");
+    await em.selectEmployeeByName("Bernice Ortiz");
+    let employee3 = await em.getEmployeeInfo();
+    expect(employee3.name).toBe("Bernice Ortiz");
+    expect(employee3.title).toBe("CEO");
+  });
+});
 });
